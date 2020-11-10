ebac2d3a00e5e8dfd7c1fea0a2f9afd9b1ae95ba
Omar <omsi96@users.noreply.github.com>
2020-11-10 05:04:28 +0300

    Items list awith desription done

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -1,8 +1,23 @@
-const { ItemStyled, ItemImage } = require("../../styles/ItemsStyled");
-
-const Item = ({ item }) => (
-  <ItemStyled enabled={true}>
-    <ItemImage src={item.img} />
-  </ItemStyled>
-);
+import { useState } from "react";
+const {
+  ItemStyled,
+  ItemImage,
+  ItemDescription,
+} = require("../../styles/ItemsStyled");
+const Item = ({ item }) => {
+  const [hovered, setHovered] = useState(false);
+  return (
+    <ItemStyled
+      enabled={item.enabled}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <ItemImage src={item.img} />
+      <ItemDescription hovered={hovered}>
+        <h3>Grandma</h3>
+        <p> She makes cookie!</p>
+      </ItemDescription>
+    </ItemStyled>
+  );
+};
 export default Item;
--- src/styles/ItemsStyled.js
+++ src/styles/ItemsStyled.js
@@ -1,29 +1,43 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const ItemStyled = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   background: #fff;
   box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.12);
-  border-radius: 50%;
+  border-radius: 90px;
   opacity: ${(props) => (props.enabled ? 1 : 0.2)};
   margin: 30px;
   width: 180px;
   height: 180px;
-  transition: 25ms all;
+  transition: 150ms all;
+  transition-timing-function: ease-in-out;
 
   &:hover {
-    width: 400px;
+    width: 490px;
+    transition: 150ms all;
+    transition-timing-function: ease-in-out;
+    cursor: pointer;
   }
-`
+`;
 
 export const ItemImage = styled.img`
-  width: 50%;
-`
+  margin-left: 44px;
+  width: 88px;
+`;
+
+export const ItemDescription = styled.div`
+  opacity: ${(props) => (props.hovered ? "1" : "0")};
+  flex-direction: column;
+  transition-timing-function: ease-in-out;
+  transition: 100ms all;
+  margin-left: 10px;
+`;
 
 export const ItemsListStyled = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  margin: 10px;
+`;
